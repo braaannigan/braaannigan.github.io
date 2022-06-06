58570ead8e9e7eb9f00795e62093bb51c560208d
Liam Brannigan <l.brannigan@analyticsengines.com>
2022-06-06 16:49:57 +0100

    add other files"

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,15 @@
 #   Copyright (C) 2020 Liam Brannigan
 #
 #Target can be 'dev', 'test' or 'deploy' as per Dockerfile stages
-TARGET=$1
+docker run -i -t --rm -u 1000:1000 -p 4000:4000 -v $(pwd):/opt/app -v $(pwd)/.bundler/:/opt/bundler -e BUNDLE_PATH=/opt/bundler -w /opt/app ruby:2.7.6 bash -c "bundle install && bundle exec jekyll serve --watch -H 0.0.0.0"
+# TARGET=$1
 
-docker build -t braaannigan-github-pages-jekyll .
+# docker build -t braaannigan-github-pages-jekyll .
 
-if [ "${TARGET}" == "dev" ]; then
-docker run -v $(pwd):/usr/src/app  -p 4000:4000 braaannigan-github-pages-jekyll bundle exec jekyll serve --host 0.0.0.0
+# if [ "${TARGET}" == "dev" ]; then
+# docker run -v $(pwd):/usr/src/app  -p 4000:4000 braaannigan-github-pages-jekyll bundle exec jekyll serve --host 0.0.0.0
 
-fi
+# fi
 
 # if [ "${TARGET}" == "lab" ]; then
 # docker run -p 8890:8888 -it --rm  -v $(pwd):/usr/src/app braaannigan-github-pages-jekyll:latest /bin/bash
